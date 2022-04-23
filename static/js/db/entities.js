0c67a6d620b524a42e9318df599df59e160f4e39
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-04-23 23:50:24 +0300

    Changed entities to include note key

--- static/js/db/entities.js
+++ static/js/db/entities.js
@@ -1,5 +1,6 @@
 class Content {
-    constructor(title, timestamp, type, tag, pinned) {
+    constructor(key, title, timestamp, type, tag, pinned) {
+        this.key = key;
         this.title = title;
         this.timestamp = timestamp;
         this.type = type;
@@ -9,15 +10,15 @@ class Content {
 }
 
 export class Note extends Content {
-    constructor(title, timestamp, type, tag, pinned, content) {
-        super(title, timestamp, type, tag, pinned);
+    constructor(key, title, timestamp, type, tag, pinned, content) {
+        super(key, title, timestamp, type, tag, pinned);
         this.content = content;
     }
 }
 
 export class List extends Content {
-    constructor(title, timestamp, type, tag, pinned, listItems) {
-        super(title, timestamp, type, tag, pinned);
+    constructor(key, title, timestamp, type, tag, pinned, listItems) {
+        super(key, title, timestamp, type, tag, pinned);
         this.listItems = listItems;
     }
 }
